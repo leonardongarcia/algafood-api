2ea2255ed9b3a26cbdaeb7297f6cb47a9f1c09b0
Leonardo do N. Garcia <leonprogramacao@gmail.com>
2022-10-03 12:27:51 -0300

    Configuração dos filtros da web

---
+++ src/main/java/com/garcia/algafoodapi/core/web/WebConfig.java
@@ -0,0 +1,17 @@
+package com.garcia.algafoodapi.core.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Bean
+  public Filter shallowEtagHeaderFilter() {
+    return new ShallowEtagHeaderFilter();
+  }
+}
