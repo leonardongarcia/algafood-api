d5d0c2026706ffd3614a0376dd95f83b6efae366
Leonardo do N. Garcia <leonprogramacao@gmail.com>
2022-10-03 12:26:51 -0300

    Configuração do armazenamento de arquivos

---
+++ src/main/java/com/garcia/algafoodapi/core/storage/StorageConfig.java
@@ -0,0 +1,44 @@
+package com.garcia.algafoodapi.core.storage;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.garcia.algafoodapi.domain.service.FotoStorageService;
+import com.garcia.algafoodapi.infrastructure.service.storage.LocalFotoStorageService;
+import com.garcia.algafoodapi.infrastructure.service.storage.S3FotoStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static com.garcia.algafoodapi.core.storage.StorageProperties.TipoStorage;
+
+@Configuration
+public class StorageConfig {
+
+  @Autowired private StorageProperties storageProperties;
+
+  @Bean
+  @ConditionalOnProperty(name = "algafood.storage.tipo", havingValue = "s3")
+  public AmazonS3 amazonS3() {
+    var credentials =
+        new BasicAWSCredentials(
+            storageProperties.getS3().getIdChaveAcesso(),
+            storageProperties.getS3().getChaveAcessoSecreta());
+
+    return AmazonS3ClientBuilder.standard()
+        .withCredentials(new AWSStaticCredentialsProvider(credentials))
+        .withRegion(storageProperties.getS3().getRegiao())
+        .build();
+  }
+
+  @Bean
+  public FotoStorageService fotoStorageService() {
+    if (TipoStorage.S3.equals(storageProperties.getTipo())) {
+      return new S3FotoStorageService();
+    } else {
+      return new LocalFotoStorageService();
+    }
+  }
+}
