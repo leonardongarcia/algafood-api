94bc9bc490d5702c7916e88053b952feb34634fe
Leonardo do N. Garcia <leonprogramacao@gmail.com>
2022-10-04 12:55:32 -0300

    Configurando o resource server para ler as authorities do token

--- src/main/java/com/garcia/algafoodapi/core/security/ResourceServerConfig.java
+++ src/main/java/com/garcia/algafoodapi/core/security/ResourceServerConfig.java
@@ -6,7 +6,16 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -25,8 +34,34 @@ public class ResourceServerConfig {
         .cors()
         .and()
         .oauth2ResourceServer()
-        .jwt();
+        .jwt()
+        .jwtAuthenticationConverter(jwtAuthenticationConverter());
 
     return httpSecurity.formLogin(Customizer.withDefaults()).build();
+  }
+
+  private JwtAuthenticationConverter jwtAuthenticationConverter() {
+    JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+
+    converter.setJwtGrantedAuthoritiesConverter(
+        jwt -> {
+          List<String> authorities = jwt.getClaimAsStringList("authorities");
+
+          if (authorities == null) {
+            return Collections.emptySet();
+          }
+
+          JwtGrantedAuthoritiesConverter authoritiesConverter =
+              new JwtGrantedAuthoritiesConverter();
+
+          Collection<GrantedAuthority> grantedAuthorities = authoritiesConverter.convert(jwt);
+
+          grantedAuthorities.addAll(
+              authorities.stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+
+          return grantedAuthorities;
+        });
+
+    return converter;
   }
 }
