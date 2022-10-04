106b5268311c60288d4b458ea93ff52ba2d3457d
Leonardo do N. Garcia <leonprogramacao@gmail.com>
2022-10-04 11:17:02 -0300

    Incluindo os fluxo de autorização, authorization code e PKCE

--- src/main/java/com/garcia/algafoodapi/core/security/ResourceServerConfig.java
+++ src/main/java/com/garcia/algafoodapi/core/security/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.garcia.algafoodapi.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,6 +27,6 @@ public class ResourceServerConfig {
         .oauth2ResourceServer()
         .jwt();
 
-    return httpSecurity.build();
+    return httpSecurity.formLogin(Customizer.withDefaults()).build();
   }
 }
--- src/main/java/com/garcia/algafoodapi/core/security/authorizationserver/JpaUserDetailService.java
+++ src/main/java/com/garcia/algafoodapi/core/security/authorizationserver/JpaUserDetailService.java
@@ -5,6 +5,7 @@ import com.garcia.algafoodapi.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -29,7 +30,7 @@ public class JpaUserDetailService implements UserDetailsService {
             .orElseThrow(
                 () -> new UsernameNotFoundException("Usuário não encontrado com e-mail informado"));
 
-    return new AuthUser(usuario, getAuthorities(usuario));
+    return new User(usuario.getEmail(), usuario.getSenha(), getAuthorities(usuario));
   }
 
   private Collection<GrantedAuthority> getAuthorities(Usuario usuario) {
