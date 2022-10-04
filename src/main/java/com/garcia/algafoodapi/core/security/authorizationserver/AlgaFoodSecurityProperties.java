52d615d1452a1a635ccbc7504a286947d3a3e53d
Leonardo do N. Garcia <leonprogramacao@gmail.com>
2022-10-04 09:45:31 -0300

    Configurando o authorization server do spring

---
+++ src/main/java/com/garcia/algafoodapi/core/security/authorizationserver/AlgaFoodSecurityProperties.java
@@ -0,0 +1,19 @@
+package com.garcia.algafoodapi.core.security.authorizationserver;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+
+@Component
+@Getter
+@Setter
+@Validated
+@ConfigurationProperties("algafood.auth")
+public class AlgaFoodSecurityProperties {
+
+  @NotBlank private String providerUrl;
+}
