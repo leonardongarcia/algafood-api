39767d26ba71eb589b219bf360bf1ae13d1663fe
Leonardo do N. Garcia <leonprogramacao@gmail.com>
2022-10-04 14:32:25 -0300

    Customizando a página de consentimento do OAuth2

---
+++ src/main/java/com/garcia/algafoodapi/core/security/authorizationserver/AuthorizationConsentController.java
@@ -0,0 +1,64 @@
+package com.garcia.algafoodapi.core.security.authorizationserver;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsent;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.nio.file.AccessDeniedException;
+import java.security.Principal;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@Controller
+@RequiredArgsConstructor
+public class AuthorizationConsentController {
+
+  private final RegisteredClientRepository registeredClientRepository;
+  private final OAuth2AuthorizationConsentService consentService;
+
+  @GetMapping("/oauth2/consent")
+  public String consent(
+      Principal principal,
+      Model model,
+      @RequestParam(OAuth2ParameterNames.CLIENT_ID) String clienteId,
+      @RequestParam(OAuth2ParameterNames.SCOPE) String scope,
+      @RequestParam(OAuth2ParameterNames.STATE) String state)
+      throws AccessDeniedException {
+
+    RegisteredClient client = this.registeredClientRepository.findByClientId(clienteId);
+
+    if (client == null) {
+      throw new AccessDeniedException(String.format("Cliente de %s não foi encontrado", clienteId));
+    }
+
+    OAuth2AuthorizationConsent consent =
+        this.consentService.findById(client.getClientId(), principal.getName());
+    String[] scopeArray = StringUtils.delimitedListToStringArray(scope, "");
+    Set<String> scopeParaAprovar = new HashSet<>(Set.of(scopeArray));
+
+    Set<String> scopesAprovadosAnteriormente;
+
+    if (consent != null) {
+      scopesAprovadosAnteriormente = consent.getScopes();
+      scopeParaAprovar.removeAll(scopesAprovadosAnteriormente);
+    } else {
+      scopesAprovadosAnteriormente = Collections.emptySet();
+    }
+
+    model.addAttribute("clientId", clienteId);
+    model.addAttribute("state", state);
+    model.addAttribute("principalName", principal.getName());
+    model.addAttribute("scopesParaAprovar", scopeParaAprovar);
+    model.addAttribute("scopesAprovadosAnteriormente", scopesAprovadosAnteriormente);
+    return "pages/approval";
+  }
+}
--- src/main/java/com/garcia/algafoodapi/core/security/authorizationserver/AuthorizationServerConfig.java
+++ src/main/java/com/garcia/algafoodapi/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -13,14 +13,16 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcOperations;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationConsentService;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
@@ -28,6 +30,7 @@ import org.springframework.security.oauth2.server.authorization.config.ProviderS
 import org.springframework.security.oauth2.server.authorization.token.JwtEncodingContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import java.io.InputStream;
 import java.security.KeyStore;
@@ -40,7 +43,26 @@ public class AuthorizationServerConfig {
   @Bean
   @Order(Ordered.HIGHEST_PRECEDENCE)
   public SecurityFilterChain authFilterChain(HttpSecurity httpSecurity) throws Exception {
-    OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(httpSecurity);
+    //    OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(httpSecurity);
+    OAuth2AuthorizationServerConfigurer<HttpSecurity> authorizationServerConfigurer =
+        new OAuth2AuthorizationServerConfigurer();
+
+    authorizationServerConfigurer.authorizationEndpoint(
+        customizer -> customizer.consentPage("/oauth2/consent"));
+
+    RequestMatcher endpointsMatcher = authorizationServerConfigurer.getEndpointsMatcher();
+    httpSecurity
+        .requestMatcher(endpointsMatcher)
+        .authorizeRequests(
+            (authorizeRequests) -> {
+              ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl) authorizeRequests.anyRequest())
+                  .authenticated();
+            })
+        .csrf(
+            (csrf) -> {
+              csrf.ignoringRequestMatchers(endpointsMatcher);
+            })
+        .apply(authorizationServerConfigurer);
     return httpSecurity.formLogin(customizer -> customizer.loginPage("/login")).build();
   }
 
@@ -96,5 +118,11 @@ public class AuthorizationServerConfig {
         context.getClaims().claim("authorities", authorities);
       }
     };
+  }
+
+  @Bean
+  public OAuth2AuthorizationConsentService consentService(
+      JdbcOperations jdbcOperations, RegisteredClientRepository clientRepository) {
+    return new JdbcOAuth2AuthorizationConsentService(jdbcOperations, clientRepository);
   }
 }
